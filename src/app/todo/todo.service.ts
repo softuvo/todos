0bb617a72b4aa42a5af09195e3bad078bbcb598a
Softuvo second local server <ravinder.kumar@softuvo.com>
2020-02-20 11:44:08 +0000

    Initial commit

---
+++ src/app/todo/todo.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+
+let TODOS = [
+  { title: 'Install Angular CLI', isDone: true },
+  { title: 'Style app', isDone: true },
+  { title: 'Finish service functionality', isDone: false },
+  { title: 'Setup API', isDone: false },
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor() { }
+
+  get(query = '') {
+    return new Promise(resolve => {
+      let data;
+
+      data = TODOS;
+
+      resolve(data);
+    });
+  }
+
+  add(data) {
+    return new Promise(resolve => {
+      TODOS.push(data);
+      resolve(data);
+    });
+  }
+
+  put(changed) {
+    return new Promise(resolve => {
+      const index = TODOS.findIndex(todo => todo === changed);
+      TODOS[index].title = changed.title;
+      resolve(changed);
+    });
+  }
+
+  delete(selected) {
+    return new Promise(resolve => {
+      const index = TODOS.findIndex(todo => todo === selected);
+      TODOS.splice(index, 1);
+      resolve(true);
+    });
+  }
+
+  deleteCompleted() {
+    return new Promise(resolve => {
+      TODOS = TODOS.filter(todo => !todo.isDone);
+      resolve(TODOS);
+    });
+  }
+
+  toggle(selected) {
+    selected.isDone = !selected.isDone;
+    return Promise.resolve();
+  }
+
+}
